ae7a9f6106bd14ea59f547652e6c9d5e1466da86
Nick Tremblay <NickSB2000@gmail.com>
2019-05-15 20:05:32 -0400

    Very Bad merge, Ugly code, but funtionnal proof, ported to python for windows

--- i2ne-pl-to-folder.py
+++ i2ne-pl-to-folder.py
@@ -1,5 +1,3 @@
-#!/bin/env python3
-
 import os
 import sys
 import glob
@@ -41,9 +39,21 @@ except:
 
 # Functions()
 def osCommand(command):
-  return subprocess.run(command, stdout=subprocess.PIPE, stderr=FNULL).stdout.decode('utf-8').strip()
+  if os.name == 'nt':
+    COMMAND = command
+  else:
+    COMMAND = shlex.split(command)
+  return subprocess.run(COMMAND, stdout=subprocess.PIPE, stderr=FNULL).stdout.decode('utf-8').strip()
 
 def path_to_cygwin(inpath):
+  if os.name == 'nt':
+    if inpath.lower().startswith('desktop'):
+      wdesktopPath = os.path.join(os.environ["HOMEPATH"], "Desktop")
+      return (os.path.isdir(wdesktopPath), wdesktopPath)
+    if inpath.startswith('/'):
+      return (os.path.isfile(inpath[1:]), inpath[1:])
+    else:
+      return (os.path.isfile(inpath), inpath)
   if inpath.startswith('/'):
     INPATH = inpath[1:]
   else:
@@ -56,140 +66,184 @@ def path_to_cygwin(inpath):
   return (os.path.isfile(thePath), thePath)
 
 def path_to_windows(inpath):
-  INPATH = inpath
-  commd = ["cygpath.exe", "-w", INPATH ]
-  return osCommand(commd)
+  if os.name == 'nt':
+    return inpath
+  else:
+    INPATH = inpath
+    commd = ["cygpath.exe", "-w", INPATH ]
+    return osCommand(commd)
 
-# Cygwin related dependencies
-if len(osCommand(shlex.split('which ldd'))) < 2:
-  print('\nError: please make sure that the ldd utility is included when installing cygwin\nAborting!\n')
-  sys.exit(1) 
+def get_drives():
+  if os.name == 'nt':
+    import ctypes
+    buff_size = ctypes.windll.kernel32.GetLogicalDriveStringsW(0,None)
+    buff = ctypes.create_string_buffer(buff_size*2)
+    x = ctypes.windll.kernel32.GetLogicalDriveStringsW(buff_size,buff)
+    iterable = filter(None, buff.raw.decode('utf-16-le').split(u'\0'))
+    return list(iterable)
+  else:
+    return glob.glob('/cygdrive/*')
 
-checkffmpeg = osCommand(shlex.split('which ffmpeg'))
-if len(checkffmpeg) < 2:
-  print('\nError: please make sure that ffmpeg is installed\nAborting!\n')
-  sys.exit(1) 
+ffmpegLocation = 'ffmpeg'
+if os.name == 'nt':
+  print("Looking for 'ffmpeg.exe' on your C: Drive, one moment...")
+  ffmpegLocations = glob.glob('c:/**/ffmpeg.exe', recursive=True)
+  if ffmpegLocations.__len__() == 0:
+    print("Please put ffmpeg.exe somewhere on your C: drive..")
+    print("You can get it from there: https://ffmpeg.zeranoe.com/builds/")
+    print("Install it and restart this program!, Aborting for now..\n")
+    sys.exit()
+  if ffmpegLocations.__len__() == 1:
+    ffmpegLocation = ffmpegLocations[0]
+  else:
+    selection = SelectionMenu(ffmpegLocations, title='Which one do you choose?', subtitle='Please Select one:')
+    selection.show()
+    ffmpegLocation = ffmpegLocations[selection.returned_value]
+  #print(ffmpegLocation)
+else:
+  # Cygwin related dependencies
+  if len(osCommand(shlex.split('which ldd'))) < 2:
+    print('\nError: please make sure that the ldd utility is included when installing cygwin\nAborting!\n')
+    sys.exit(1) 
 
-ffmpegWinCmdOut = osCommand(shlex.split('ldd %s' % checkffmpeg))
-if not ffmpegWinCmdOut.__contains__('dll'):
-  print('\nError: please make sure that %s is Windows version/built\nAborting!\n' % checkffmpeg)
-  sys.exit(1) 
+    checkffmpeg = osCommand(shlex.split('which ffmpeg'))
+    if len(checkffmpeg) < 2:
+      print('\nError: please make sure that ffmpeg is installed\nAborting!\n')
+      sys.exit(1) 
 
-searchresult = osCommand(shlex.split("which find"))
-if len(searchresult) < 2: 
-  print('\nError: please make sure that the find utility is included when installing cygwin\nAborting!\n')
-  sys.exit(1)
+    ffmpegWinCmdOut = osCommand(shlex.split('ldd %s' % checkffmpeg))
+    if not ffmpegWinCmdOut.__contains__('dll'):
+      print('\nError: please make sure that %s is Windows version/built\nAborting!\n' % checkffmpeg)
+      sys.exit(1) 
+
+    searchresult = osCommand(shlex.split("which find"))
+    if len(searchresult) < 2: 
+      print('\nError: please make sure that the find utility is included when installing cygwin\nAborting!\n')
+      sys.exit(1)
 
 
 status , userpath = path_to_cygwin(os.environ['USERPROFILE'])
 itunesDefaultLibrary = userpath + '/Music/iTunes/iTunes Music Library.xml'
+
+if os.name == 'nt':
+  itunesDefaultLibrary = itunesDefaultLibrary.replace('/','\\')
+
+if not os.path.isfile(itunesDefaultLibrary):
+  print("\nError: Itunes Library File '%s' not found\nAborting!\n" % itunesDefaultLibrary)
+  sys.exit()
 
 plist = plistlib.readPlist(itunesDefaultLibrary)
 
 playlists = []
 for plObj in plist['Playlists']:
   playlists.append(plObj['Name'])
-  
-selection_menu = SelectionMenu(playlists, title=itunesDefaultLibrary, subtitle='Please Select a Library:')
-selection_menu.show()
 
-try:
-  SelectedPlaylist = plist['Playlists'][selection_menu.returned_value]['Name']
-  trackList = plist['Playlists'][selection_menu.returned_value]['Playlist Items']
-  #print(plist['Playlists'][selection_menu.returned_value])
-except:
-  # if you're here, means that you've hit exit at the selection menu
-  sys.exit(0)
+while True:  
+  selection_menu = SelectionMenu(playlists, title=itunesDefaultLibrary, subtitle='Please Select a Library:')
+  selection_menu.show()
 
-folderCounter = 0
-exist, desktopFolder = path_to_cygwin('Desktop')
-PLAYLIST_FOLDER=''
-for fnum in range(1,100):
-  folderCounter += 1
-  PLAYLIST_FOLDER = os.path.join(desktopFolder, "%s_%02d" % ( slugify(SelectedPlaylist, separator="_"), folderCounter))
-  if os.path.isdir(PLAYLIST_FOLDER): 
-    if os.listdir(PLAYLIST_FOLDER):
-      continue
-  else:
-    os.mkdir(PLAYLIST_FOLDER)
-  print("\n Playlist will be saved to:\n '%s'\n" % PLAYLIST_FOLDER)
-  break
-  
-
-counter = 0
-speculativeDrive = ''
-for item in trackList:
-  counter += 1
   try:
-    TRACK       = str(item['Track ID'])
-    URIlocation = plist['Tracks'][TRACK]['Location']
-    trackName   = slugify(plist['Tracks'][TRACK]['Name'], separator="_", max_length=80, word_boundary=True)
-    trackArtist = slugify(plist['Tracks'][TRACK]['Artist'], separator="_", max_length=20, word_boundary=True)
-    trackAlbum  = slugify(plist['Tracks'][TRACK]['Album'], separator="_", max_length=20, word_boundary=True)
+    SelectedPlaylist = plist['Playlists'][selection_menu.returned_value]['Name']
+    trackList = plist['Playlists'][selection_menu.returned_value]['Playlist Items']
+    #print(plist['Playlists'][selection_menu.returned_value])
   except:
-    continue
-  url_OBJ = urlparse(plist['Tracks'][TRACK]['Location'])
-  if url_OBJ.scheme.lower() == 'file':
-    ifexist, LOCATION = path_to_cygwin(unquote(url_OBJ.path))
-    EXTENSION = os.path.splitext(LOCATION)[1]
-    if not ifexist:
-      print(" Oups File not found: %s" % LOCATION) 
-      print(" Trying to find it.. It could be on a differernt drive..")
-      searchresult = ''
-      if speculativeDrive != '':
-        iterateOverDriveList = [speculativeDrive] + glob.glob('/cygdrive/*')
-      else:
-        iterateOverDriveList = glob.glob('/cygdrive/*')
-      for drive in iterateOverDriveList:
-        print(" Trying to find it on drive '%s', one moment.. " % drive)
-        searchCommand = 'find %s -type f -name "%s" -print -quit' % (drive, os.path.basename(LOCATION))
-        searchresult = osCommand(shlex.split(searchCommand)) 
-        if os.path.isfile(searchresult):
-          print(" ---> FOUND: %s " % searchresult)
-          LOCATION = searchresult
-          speculativeDrive = drive
-          break
-      if searchresult == '':
-        LOCATION = "NOT FOUND"
-  elif url_OBJ.scheme.lower().startswith('http'):
-    LOCATION  = plist['Tracks'][TRACK]['Location']
-    EXTENSION = os.path.splitext(url_OBJ.path)[1]
-  else:
-    LOCATION = "NOT DETECTED"
-  newFileName = "%04d__%s_%s_%s%s" % (counter, trackName, trackArtist, trackAlbum, EXTENSION)
-  if LOCATION.startswith('NOT'):
-    print(" Can't create File: '%s' Reason: '%s'" % (newFileName, LOCATION))
-    continue
-  print(" Copying   : %s" % LOCATION)
-  print(" New file  : %s" % newFileName)
-  destination = os.path.join(PLAYLIST_FOLDER, newFileName)
-  if LOCATION.startswith('http'):
-    print('  ', end='', flush=True)
-    wget.download(LOCATION, out=destination)
-    print()
-  else:
-    shutil.copy2(LOCATION, destination)
-  newFileName     = os.path.basename(destination).replace('__','___')
-  newFileNamePath = os.path.join(PLAYLIST_FOLDER, newFileName)
-  print(" Extracting Album Art..")
-  extrCmd = 'ffmpeg -i "%s" "%s.jpg"' % (path_to_windows(destination), path_to_windows(destination))
-  osCommand(shlex.split(extrCmd))
-  print(" Removing All Tags..")
-  ffCommand = 'ffmpeg -i "%s" -vn -codec:a copy -map_metadata -1 "%s"' % (path_to_windows(destination), path_to_windows(newFileNamePath) )
-  osCommand(shlex.split(ffCommand))
-  if os.path.exists(destination + '.jpg'):
-    print(" Retaking Album art only..")
-    os.remove(destination)
-    finalInsertCmd = 'ffmpeg -i "%s" -i "%s.jpg" -map 0:0 -map 1:0 -c copy -id3v2_version 3 -metadata:s:v title="Album cover" -metadata:s:v comment="Cover (front)" "%s"' % (path_to_windows(newFileNamePath), path_to_windows(destination), path_to_windows(destination))
-    osCommand(shlex.split(finalInsertCmd))
-    os.remove(destination + '.jpg')
-    os.remove(newFileNamePath)
-  else:
-    os.remove(destination)
-    os.rename(newFileNamePath, destination)
-  #sys.exit()
-  print('\n') 
-   
+    # if you're here, means that you've hit exit at the selection menu
+    sys.exit(0)
 
+  folderCounter = 0
+  exist, desktopFolder = path_to_cygwin('Desktop')
+  PLAYLIST_FOLDER=''
+  for fnum in range(1,100):
+    folderCounter += 1
+    PLAYLIST_FOLDER = os.path.join(desktopFolder, "%s_%02d" % ( slugify(SelectedPlaylist, separator="_"), folderCounter))
+    if os.path.isdir(PLAYLIST_FOLDER): 
+      if os.listdir(PLAYLIST_FOLDER):
+        continue
+    else:
+      pass
+      os.mkdir(PLAYLIST_FOLDER)
+    print("\n Playlist will be saved to:\n '%s'\n" % PLAYLIST_FOLDER)
+    break
+    
+  counter = 0
+  speculativeDrive = ''
+  for item in trackList:
+    counter += 1
+    try:
+      TRACK       = str(item['Track ID'])
+      URIlocation = plist['Tracks'][TRACK]['Location']
+      trackName   = slugify(plist['Tracks'][TRACK]['Name'], separator="_", max_length=80, word_boundary=True)
+      trackArtist = slugify(plist['Tracks'][TRACK]['Artist'], separator="_", max_length=20, word_boundary=True)
+      trackAlbum  = slugify(plist['Tracks'][TRACK]['Album'], separator="_", max_length=20, word_boundary=True)
+    except:
+      continue  
+    #print(plist['Tracks'][TRACK]['Location'])
+    url_OBJ = urlparse(plist['Tracks'][TRACK]['Location'])
+    if url_OBJ.scheme.lower() == 'file':
+      ifexist, LOCATION = path_to_cygwin(unquote(url_OBJ.path))
+      EXTENSION = os.path.splitext(LOCATION)[1]
+      if not ifexist:
+        print(" Oups File not found: %s" % LOCATION) 
+        print(" Trying to find it.. It could be on a differernt drive..")
+        searchresult = ''
+        if speculativeDrive != '':
+          iterateOverDriveList = [speculativeDrive] + get_drives()
+        else:
+          iterateOverDriveList = get_drives()
+        for drive in iterateOverDriveList:
+          print(" Trying to find it on drive '%s', one moment.. " % drive)
+          #searchCommand = 'find %s -type f -name "%s" -print -quit' % (drive, os.path.basename(LOCATION))
+          #searchresult = osCommand(searchCommand) 
+          search = glob.glob('%s/**/%s' % (drive, os.path.basename(LOCATION)), recursive=True)
+          try:
+            searchresult = search[0]
+          except:
+            searchresult = ''
+          if os.path.isfile(searchresult):
+            print(" ---> FOUND: %s " % searchresult)
+            LOCATION = searchresult
+            speculativeDrive = drive
+            break
+        if searchresult == '':
+          LOCATION = "NOT FOUND"
+          sys.exit()
+    elif url_OBJ.scheme.lower().startswith('http'):
+      LOCATION  = plist['Tracks'][TRACK]['Location']
+      EXTENSION = os.path.splitext(url_OBJ.path)[1]
+    else:
+      LOCATION = "NOT DETECTED"
+    newFileName = "%04d__%s_%s_%s%s" % (counter, trackName, trackArtist, trackAlbum, EXTENSION)
+    if LOCATION.startswith('NOT'):
+      print(" Can't create File: '%s' Reason: '%s'" % (newFileName, LOCATION))
+      continue
+    print(" Copying   : %s" % LOCATION)
+    print(" New file  : %s" % newFileName)
+    destination = os.path.join(PLAYLIST_FOLDER, newFileName)
+    if LOCATION.startswith('http'):
+      print('  ', end='', flush=True)
+      wget.download(LOCATION, out=destination)
+      print()
+    else:
+      shutil.copy2(LOCATION, destination)
+    newFileName     = os.path.basename(destination).replace('__','___')
+    newFileNamePath = os.path.join(PLAYLIST_FOLDER, newFileName)
+    print(" Extracting Album Art..")
+    extrCmd = ffmpegLocation + ' -i "%s" "%s.jpg"' % (path_to_windows(destination), path_to_windows(destination))
+    osCommand(extrCmd)
+    print(" Removing All Tags..")
+    ffCommand = ffmpegLocation + ' -i "%s" -vn -codec:a copy -map_metadata -1 "%s"' % (path_to_windows(destination), path_to_windows(newFileNamePath) )
+    osCommand(ffCommand)
+    if os.path.exists(destination + '.jpg'):
+      print(" Retaking Album art only..")
+      os.remove(destination)
+      finalInsertCmd = ffmpegLocation + ' -i "%s" -i "%s.jpg" -map 0:0 -map 1:0 -c copy -id3v2_version 3 -metadata:s:v title="Album cover" -metadata:s:v comment="Cover (front)" "%s"' % (path_to_windows(newFileNamePath), path_to_windows(destination), path_to_windows(destination))
+      osCommand(finalInsertCmd)
+      os.remove(destination + '.jpg')
+      os.remove(newFileNamePath)
+    else:
+      os.remove(destination)
+      os.rename(newFileNamePath, destination)
+    print('\n') 
+   
 
   
