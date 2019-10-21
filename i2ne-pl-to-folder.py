e219a9c45c05f75998bb06f71810ed68157be1ad
Nick Tremblay <NickSB2000@gmail.com>
2019-10-20 21:26:23 -0400

    lots of improvements

--- i2ne-pl-to-folder.py
+++ i2ne-pl-to-folder.py
@@ -1,132 +1,135 @@
+#!/usr/bin/python3
+
 import os
 import sys
+import time
 import glob
+import json
 import shlex
 import shutil
 import plistlib
 import subprocess
+import pickle
 
 from urllib.parse import urlparse, unquote
 
 # we need a /dev/null
 FNULL = open(os.devnull, 'w')
 
+# Path of current file for options
+picklePath = os.path.join(os.path.dirname(os.path.realpath(__file__)), "opts.pkl")
+
 # Import the necessary packages and give nice errors
 try:
   import eyed3
 except:
-  print('Error: please : pip install eyeD3')
+  print('Error: from command prompt as admin : pip install eyeD3')
+  input()
   sys.exit(1)
 
 try:
   from cursesmenu import *
   from cursesmenu.items import *
 except:
-  print('Error: please : pip install curses-menu')
+  print('Error: from command prompt as admin : pip install curses-menu')
+  input()
   sys.exit(1)
 
 try:
   import wget
 except:
-  print('Error: please : pip3 install wget')
+  print('Error: from command prompt as admin : pip3 install wget')
+  input()
   sys.exit(1)
 
 try:
   from slugify import slugify
 except:
-  print('Error: please : pip3 install python-slugify')
+  print('Error: from command prompt as admin : pip3 install python-slugify')
+  input()
   sys.exit(1)
+
+
+#-----------------
+
+
+#-----------------
+
+
 
 # Functions()
 def osCommand(command):
-  if os.name == 'nt':
-    COMMAND = command
-  else:
-    COMMAND = shlex.split(command)
-  return subprocess.run(COMMAND, stdout=subprocess.PIPE, stderr=FNULL).stdout.decode('utf-8').strip()
+  return subprocess.run(command, stdout=subprocess.PIPE, stderr=FNULL).stdout.decode('utf-8').strip()
 
-def path_to_cygwin(inpath):
-  if os.name == 'nt':
-    if inpath.lower().startswith('desktop'):
-      wdesktopPath = os.path.join(os.environ["HOMEPATH"], "Desktop")
-      return (os.path.isdir(wdesktopPath), wdesktopPath)
-    if inpath.startswith('/'):
-      return (os.path.isfile(inpath[1:]), inpath[1:])
-    else:
-      return (os.path.isfile(inpath), inpath)
-  if inpath.startswith('/'):
-    INPATH = inpath[1:]
-  else:
-    INPATH = inpath
+def path_resolver(inpath):
   if inpath.lower().startswith('desktop'):
-    commd = ["cygpath.exe", "-D"]
+    wdesktopPath = os.path.join(os.environ["HOMEPATH"], "Desktop")
+    return (os.path.isdir(wdesktopPath), wdesktopPath)
+  if inpath.startswith('/'):
+    return (os.path.isfile(inpath[1:]), inpath[1:])
   else:
-    commd = ["cygpath.exe", "-u", INPATH ]
-  thePath = osCommand(commd)
-  return (os.path.isfile(thePath), thePath)
+    return (os.path.isfile(inpath), inpath)
 
 def path_to_windows(inpath):
-  if os.name == 'nt':
-    return inpath
-  else:
-    INPATH = inpath
-    commd = ["cygpath.exe", "-w", INPATH ]
-    return osCommand(commd)
+  return inpath
 
 def get_drives():
-  if os.name == 'nt':
-    import ctypes
-    buff_size = ctypes.windll.kernel32.GetLogicalDriveStringsW(0,None)
-    buff = ctypes.create_string_buffer(buff_size*2)
-    x = ctypes.windll.kernel32.GetLogicalDriveStringsW(buff_size,buff)
-    iterable = filter(None, buff.raw.decode('utf-16-le').split(u'\0'))
-    return list(iterable)
-  else:
-    return glob.glob('/cygdrive/*')
+  import ctypes
+  buff_size = ctypes.windll.kernel32.GetLogicalDriveStringsW(0,None)
+  buff = ctypes.create_string_buffer(buff_size*2)
+  x = ctypes.windll.kernel32.GetLogicalDriveStringsW(buff_size,buff)
+  iterable = filter(None, buff.raw.decode('utf-16-le').split(u'\0'))
+  return list(iterable)
 
-ffmpegLocation = 'ffmpeg'
-if os.name == 'nt':
-  print("Looking for 'ffmpeg.exe' on your C: Drive, one moment...")
-  ffmpegLocations = glob.glob('c:/**/ffmpeg.exe', recursive=True)
+def find_ffmpeg_locations(drive='c'):
+  locations = glob.glob('%s:/**/ffmpeg.exe' % drive, recursive=True)
+  approvedl = []
+  for lfile in locations:
+    fprobefile = os.path.join(os.path.dirname(lfile), "ffprobe.exe")
+    if os.path.exists(fprobefile):
+      approvedl.append(lfile)
+  return approvedl
+
+foundFFbin = False
+while not foundFFbin:
+  gotsome = False
+  if os.path.exists(picklePath):
+    try:
+      ffmpegLocations = pickle.load( open(picklePath, "rb" ) )
+      gotsome = True
+    except:
+      pass
+
+  if gotsome == False:
+    print("Looking for 'ffmpeg.exe' / 'ffprobe.exe' on your C: Drive, one moment...")
+    ffmpegLocations = find_ffmpeg_locations()
+    if not os.path.exists(picklePath):
+      pickle.dump(ffmpegLocations, open(picklePath, "wb" ) )
+
   if ffmpegLocations.__len__() == 0:
     print("Please put ffmpeg.exe somewhere on your C: drive..")
     print("You can get it from there: https://ffmpeg.zeranoe.com/builds/")
     print("Install it and restart this program!, Aborting for now..\n")
     sys.exit()
+
   if ffmpegLocations.__len__() == 1:
     ffmpegLocation = ffmpegLocations[0]
   else:
     selection = SelectionMenu(ffmpegLocations, title='Which one do you choose?', subtitle='Please Select one:')
     selection.show()
-    ffmpegLocation = ffmpegLocations[selection.returned_value]
-  #print(ffmpegLocation)
-else:
-  # Cygwin related dependencies
-  if len(osCommand(shlex.split('which ldd'))) < 2:
-    print('\nError: please make sure that the ldd utility is included when installing cygwin\nAborting!\n')
-    sys.exit(1) 
+    ffmpegLocation  = ffmpegLocations[selection.returned_value]
+    ffprobeLocation = os.path.join(os.path.dirname(ffmpegLocation), "ffprobe.exe")
 
-    checkffmpeg = osCommand(shlex.split('which ffmpeg'))
-    if len(checkffmpeg) < 2:
-      print('\nError: please make sure that ffmpeg is installed\nAborting!\n')
-      sys.exit(1) 
-
-    ffmpegWinCmdOut = osCommand(shlex.split('ldd %s' % checkffmpeg))
-    if not ffmpegWinCmdOut.__contains__('dll'):
-      print('\nError: please make sure that %s is Windows version/built\nAborting!\n' % checkffmpeg)
-      sys.exit(1) 
-
-    searchresult = osCommand(shlex.split("which find"))
-    if len(searchresult) < 2: 
-      print('\nError: please make sure that the find utility is included when installing cygwin\nAborting!\n')
-      sys.exit(1)
+  if not os.path.exists(ffmpegLocation):
+    print("Okay, well, seems like that ffmpeg binary is gone, try again..")
+    os.remove(picklePath)
+  else:
+    foundFFbin = True
 
 
-status , userpath = path_to_cygwin(os.environ['USERPROFILE'])
+status , userpath = path_resolver(os.environ['USERPROFILE'])
 itunesDefaultLibrary = userpath + '/Music/iTunes/iTunes Music Library.xml'
-
-if os.name == 'nt':
-  itunesDefaultLibrary = itunesDefaultLibrary.replace('/','\\')
+itunesDefaultLibrary = itunesDefaultLibrary.replace('/','\\')
 
 if not os.path.isfile(itunesDefaultLibrary):
   print("\nError: Itunes Library File '%s' not found\nAborting!\n" % itunesDefaultLibrary)
@@ -151,7 +154,7 @@ while True:
     sys.exit(0)
 
   folderCounter = 0
-  exist, desktopFolder = path_to_cygwin('Desktop')
+  exist, desktopFolder = path_resolver('Desktop')
   PLAYLIST_FOLDER=''
   for fnum in range(1,100):
     folderCounter += 1
@@ -162,25 +165,45 @@ while True:
     else:
       pass
       os.mkdir(PLAYLIST_FOLDER)
-    print("\n Playlist will be saved to:\n '%s'\n" % PLAYLIST_FOLDER)
-    break
+      print("\n Playlist will be saved to:\n '%s'\n" % PLAYLIST_FOLDER)
+      break
     
   counter = 0
   speculativeDrive = ''
   for item in trackList:
     counter += 1
+    
     try:
       TRACK       = str(item['Track ID'])
+    except:
+      TRACK       = "track_%s" % counter
+
+    try:
       URIlocation = plist['Tracks'][TRACK]['Location']
+    except:
+      # If no URI location is found, the carrots are cooked
+      continue 
+
+    try:
       trackName   = slugify(plist['Tracks'][TRACK]['Name'], separator="_", max_length=80, word_boundary=True)
+    except:
+      trackName   = "_"
+
+    try:
       trackArtist = slugify(plist['Tracks'][TRACK]['Artist'], separator="_", max_length=20, word_boundary=True)
+    except:
+      trackArtist = "_"
+
+    try:
       trackAlbum  = slugify(plist['Tracks'][TRACK]['Album'], separator="_", max_length=20, word_boundary=True)
     except:
-      continue  
+      trackAlbum  = "_"
+
+
     #print(plist['Tracks'][TRACK]['Location'])
     url_OBJ = urlparse(plist['Tracks'][TRACK]['Location'])
     if url_OBJ.scheme.lower() == 'file':
-      ifexist, LOCATION = path_to_cygwin(unquote(url_OBJ.path))
+      ifexist, LOCATION = path_resolver(unquote(url_OBJ.path))
       EXTENSION = os.path.splitext(LOCATION)[1]
       if not ifexist:
         print(" Oups File not found: %s" % LOCATION) 
@@ -192,8 +215,6 @@ while True:
           iterateOverDriveList = get_drives()
         for drive in iterateOverDriveList:
           print(" Trying to find it on drive '%s', one moment.. " % drive)
-          #searchCommand = 'find %s -type f -name "%s" -print -quit' % (drive, os.path.basename(LOCATION))
-          #searchresult = osCommand(searchCommand) 
           search = glob.glob('%s/**/%s' % (drive, os.path.basename(LOCATION)), recursive=True)
           try:
             searchresult = search[0]
@@ -225,13 +246,37 @@ while True:
       print()
     else:
       shutil.copy2(LOCATION, destination)
-    newFileName     = os.path.basename(destination).replace('__','___')
-    newFileNamePath = os.path.join(PLAYLIST_FOLDER, newFileName)
+
+    winPathDestination      = path_to_windows(destination)
+    destinationNoExt, exten = os.path.splitext(destination)
+    destinationMP3          = destinationNoExt + '.mp3'
+    winPathDestinationMP3   = path_to_windows(destinationMP3)
+    newFileName             = os.path.basename(destinationMP3).replace('__','___')
+    newFileNamePath         = os.path.join(PLAYLIST_FOLDER, newFileName)
+
+    ffCommand = ffprobeLocation + ' -v quiet -print_format json -show_format -show_streams "%s"' % winPathDestination
+    osCmdOutputJSON   = osCommand(ffCommand)
+    osCmdOutputPYTHON = json.loads(osCmdOutputJSON)
+    try:
+      formatName = osCmdOutputPYTHON['format']['format_name']
+    except:
+      formatName = 'unknown'
+    print(" Format    : %s" % formatName)
+
+    if formatName.lower() != 'mp3':
+      print(" Converting: %s" % destinationMP3)
+      #ffCommand = ffmpegLocation + ' -i "%s" -acodec libmp3lame "%s"' % (winPathDestination, path_to_windows(destinationMP3) )
+      ffCommand = ffmpegLocation + ' -threads 0 -i "%s" "%s"' % (winPathDestination, winPathDestinationMP3 )
+      subprocess.run(ffCommand)
+      os.remove(destination)
+      destination = destinationMP3
+
     print(" Extracting Album Art..")
-    extrCmd = ffmpegLocation + ' -i "%s" "%s.jpg"' % (path_to_windows(destination), path_to_windows(destination))
+    winPathDestination = path_to_windows(destination)
+    extrCmd = ffmpegLocation + ' -i "%s" "%s.jpg"' % (winPathDestination, winPathDestination)
     osCommand(extrCmd)
     print(" Removing All Tags..")
-    ffCommand = ffmpegLocation + ' -i "%s" -vn -codec:a copy -map_metadata -1 "%s"' % (path_to_windows(destination), path_to_windows(newFileNamePath) )
+    ffCommand = ffmpegLocation + ' -i "%s" -vn -codec:a copy -map_metadata -1 "%s"' % (winPathDestination, path_to_windows(newFileNamePath) )
     osCommand(ffCommand)
     if os.path.exists(destination + '.jpg'):
       print(" Retaking Album art only..")
